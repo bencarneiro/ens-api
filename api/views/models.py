137236d5a62a3a5e1c6cbda1cd58a093aaa7bf02
bencarneiro <bencarneiro@gmail.com>
2023-03-18 08:23:44 -0500

    add django project, add model and migration for initial eth_domain table

---
+++ api/views/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class EthDomain(models.Model):
+
+    name_hash = models.CharField(primary_key=True, max_length=66)
+    domain_name = models.TextField()
+    registrant = models.CharField(max_length=64, blank=False, null=False)
+    expiration_date = models.DateTimeField(null=False, blank=False)
+    cost = models.BigIntegerField(null=False)
+    tx_block = models.IntegerField(null=False)
+    tx_hash = models.CharField(max_length=128, blank=False, null=False)
+    tx_dt = models.DateTimeField(null=False, blank=False)
+
+
+    class Meta:
+        managed = True
+        db_table = 'eth_domain'
